ea21404c32db180d0d24a999cabbce687cabf8f7
MadForCoding <chenw5@oregonstate.edu>
2020-05-31 17:12:59 -0500

    done ch5 5-2

--- src/ch5/findlink1/main.go
+++ src/ch5/findlink1/main.go
@@ -1,1 +1,44 @@
-package findlink1
+package main
+
+import(
+	"fmt"
+	"golang.org/x/net/html"
+	"os"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlink1: %v\n", err)
+		os.Exit(1)
+	}
+	for _, links := range visit(nil, doc){
+		fmt.Println(links)
+	}
+}
+
+
+// visit appends to links each link found in n and returns the result.
+func visit(links []string, n *html.Node) []string{
+	if n == nil {
+		return links
+	}
+	if n.Type == html.ElementNode && n.Data == "a"{
+		for _, a := range n.Attr{
+			if a.Key == "href" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+
+
+	// iterate
+	//for c := n.FirstChild; c != nil; c = c.NextSibling{
+	//	links = visit(links, c)
+	//}
+
+	// recursive  exercise5-1
+	links = visit(links, n.FirstChild)
+	links = visit(links, n.NextSibling)
+	return links
+}
--- src/ch5/outline/main.go
+++ src/ch5/outline/main.go
@@ -1,1 +1,53 @@
-package outline
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/html"
+	"os"
+)
+
+var hashmap = make(map[string]int)
+
+func main(){
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "outline: %v\n", err)
+		os.Exit(1)
+	}
+
+	outline(nil, doc)
+	fmt.Println("--------------")
+	for k,v := range hashmap{
+		fmt.Println(k,": ",v)
+	}
+}
+
+func outline(stack []string, n *html.Node){
+	if n.Type == html.ElementNode {
+
+		stack = append(stack, n.Data)
+		record(n.Data)
+		fmt.Println(stack)
+		if n.Data != "script" && n.Data != "style" {
+			printContent(stack, n.Attr)
+		}
+
+	}
+
+	for c := n.FirstChild; c !=nil ; c = c.NextSibling{
+
+		outline(stack, c)
+	}
+}
+
+// exercise 5-2
+func record(data string){
+	hashmap[data]++
+}
+
+// exercise 5-3
+func printContent(stack []string, attr []html.Attribute) {
+	for _, elem:= range attr{
+		fmt.Printf("%v: attr: %s, value: %s\n", stack, elem.Key,elem.Val)
+	}
+}
